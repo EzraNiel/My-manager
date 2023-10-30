efbc26fa03899c5f50632cacd91f5d8123cdf12a
unknown <rosomalalaed@mail.com>
2023-10-30 15:15:29 +0300

    Apropos et Contact

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,4 +1,12 @@
 import { Link } from "react-router-dom";
+import '../Components/Edwella/contact.css';
+import logo from '../Components/Edwella/Capture d’écran 2023-10-23 115608.jpg'
+import phone from '../Components/Edwella/Phone.png'
+import mail from '../Components/Edwella/Mail.png'
+import fb from '../Components/Edwella/Fb1.png'
+import mess from '../Components/Edwella/Message.png'
+
+
 
 function Contact() {
   // STATE (état,données)
@@ -8,11 +16,37 @@ function Contact() {
   //   AFFICHAGE (RENDER)
   return (
     <>
-      <h1>Contact</h1>
-      <div className="Contact">
-        <Link to="/">Acceuil</Link>
-        <Link to="/Apropos">A propos</Link>
+    <div className="Contact">
+      <div className="head">
+      <img  className="PictLogo"  src={logo} alt="logo"></img>
+      <h1 className="Page">Contact</h1>
+        <ul className="HeadList">
+        <li><Link to="/">Acceuil</Link></li>
+        <li><Link to="/Apropos">A propos</Link></li>
+        </ul>
       </div>
+      <h2>Veuillez nous contacter!</h2>
+      <div className="Section">
+        <ul>
+          <li id="phone">
+            <img src={phone}></img>
+            <h3>Téléphone</h3>
+            <text>+03.......</text>
+          </li>
+          <li id="mail">
+            <img src={mail}></img>
+            <h3>Mail</h3>
+            <text>MyManager@bmail.com</text>
+          </li>
+          <li id="fb"> <img src=""></img>
+            <img src={fb}></img>
+            <h3>Facebook</h3>
+            <text>My-Manager</text>
+          </li>
+        </ul>
+      </div>
+     
+    </div>
     </>
   );
 }
